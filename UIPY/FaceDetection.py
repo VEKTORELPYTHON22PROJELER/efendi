415bddd787dbd681778a73d3c7ec99f533a4c6ac
nurdemirbass <71544243+nurdemirbass@users.noreply.github.com>
2020-10-10 12:43:02 +0300

    ui

---
+++ UIPY/FaceDetection.py
@@ -0,0 +1,18 @@
+import sys
+from  PyQt5.QtWidgets import QApplication,QWidget
+
+from PyQt5 import uic 
+
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(r"UI\FaceDetection.ui",self)
+        self.show()
+
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
+        
---
+++ UIPY/FaceRecognition.py
@@ -0,0 +1,16 @@
+"""hello"""
+
+import sys
+from  PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5 import uic 
+
+class App(QWidget):
+    """hello"""
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(r"UI\FaceRecognition.ui",self)
+        self.show()    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())        
