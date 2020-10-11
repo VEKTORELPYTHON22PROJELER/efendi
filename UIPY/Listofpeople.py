5119bee8dfac6c4288e91b365be1e990aa82db83
ibrahim ediz <12@12.com>
2020-10-11 13:07:26 +0300

    111020

--- UIPY/Listofpeople.py
+++ UIPY/Listofpeople.py
@@ -1,19 +1,44 @@
 import sys
+sys.path.append(r"C:\Users\vektorel\Documents\GitHub\efendi\DB")
 from  PyQt5.QtWidgets import QApplication,QMainWindow
 from  PyQt5.QtGui import QIcon
-from PyQt5 import uic 
+from PyQt5 import uic
+from DB import DBTool
+
 
 class ListOfPeople(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.db = DBTool(dbAdres=r"DB\facedb.db",tabloAdi="")
+        self.ilListe = []
         self.initUI()
+        
        
 
     def initUI(self):
         uic.loadUi(r"UI\Listofpeople.ui",self)
-        # self.show()
-
+        self.cmbIlDoldur()
+        self.cmbil.currentIndexChanged.connect(self.cmbilceDoldur)
+        
     
+    def cmbIlDoldur(self):
+        self.cmbil.addItem("Seçiniz")
+        self.db.tabloAdi = "ST_ILLER"
+        self.ilListe = self.db.select()
+        for item in self.ilListe:
+            self.cmbil.addItem(item[1])
+
+    def cmbilceDoldur(self):
+        self.db.tabloAdi = "ST_ILCELER"
+        ilid = ""
+        for item in self.ilListe:
+            if item[1] == self.cmbil.currentText():
+                ilid = item[0]
+                break
+        self.cmbilce.clear()
+        self.ilceListe = self.db.select(sart=f"IL_ID={ilid}")
+        for item in self.ilceListe:
+            self.cmbilce.addItem(item[1])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
